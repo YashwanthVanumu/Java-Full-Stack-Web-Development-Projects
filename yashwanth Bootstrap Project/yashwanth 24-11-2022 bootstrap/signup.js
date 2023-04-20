398b72729e6f7a5c6d9f49b6a38814e0f9f3898b
YashwanthVanumu <126443763+YashwanthVanumu@users.noreply.github.com>
2023-04-20 12:58:34 +0530

    Add files via upload

---
+++ yashwanth Bootstrap Project/yashwanth 24-11-2022 bootstrap/signup.js
@@ -0,0 +1,33 @@
+function signup()
+{
+	<!-- Modal -->
+  "<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">"
+  "<div class="modal-dialog modal-dialog-centered">"
+    "<div class="modal-content">"
+      "<div class="modal-header">"
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Select City</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="col">
+        <a href="bootstrap1.html"><img src="hyderabad.jpg" style="height: 50px;width: 50px"><br>Hyderabad</a>
+          </div>
+          <div class="col">
+        <a href="bootstrap1.html"><img src="bengaluru.jpg" style="height: 50px;width: 50px"><br>Bengaluru</a>
+          </div>
+          <div class="col">
+        <a href="bootstrap1.html"><img src="chennai.jpg" style="height: 50px;width: 50px"><br>Chennai</a>
+          </div>
+          <div class="col">
+        <a href="bootstrap1.html"><img src="mumbai.jpg" style="height: 50px;width: 50px"><br>Mumbai</a>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+}
